d23f00bb14ebfe6c68aa48e7f52511c401793c8a
Clayton Skaggs <desertcow07@gmail.com>
2024-04-09 22:08:18 -0700

    setup client and got test home page running

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+//* Bouldering League Web App
+//* By Clayton Skaggs
+//* Version 0.1 [WW15.2]
+
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  // createHttpLink,
+} from '@apollo/client';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+// import { MaterialSymbol } from 'react-material-symbols';
+// import 'react-material-symbols/dist/rounded.css';
+
+//* Font Import
+// import "./fonts/Catamaran-Bold.ttf"
+
+//* Page Import
+
+import HomePage from './pages/HomePage';
+
+
+//* Component Import
+
+
+
+export default function App() {
+
+  // require('./css/style.css')
+  // require('./css/reset.css')
+
+  return (
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/staff" element={<Staff />} /> */}
+      </Routes>
+    </Router>
+  );
+}
---
+++ client/src/index.js
@@ -0,0 +1,35 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+import App from './App';
+import 'bootstrap';
+// import "./css/style.css";
+// import "./css/reset.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { createRoot } from 'react-dom/client';
+
+// import { LocalizationProvider } from '@mui/x-date-pickers';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+//? GraphQL Server URL
+
+const client = new ApolloClient({
+  uri: 'http://192.168.25.33:4001',
+  // uri: 'https://boardclubapp-api.up.railway.app',
+  cache: new InMemoryCache(),
+});
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <div className="application d-flex">
+    <ApolloProvider client={client}>
+      {/* <LocalizationProvider dateAdapter={AdapterDayjs}> */}
+        <App />
+      {/* </LocalizationProvider> */}
+    </ApolloProvider>
+  </div>
+);
