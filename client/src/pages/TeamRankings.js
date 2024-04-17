bdaadcf21485fc44ed8fd2b2ba71076543a8ba9c
Clayton Skaggs <desertcow07@gmail.com>
2024-04-17 14:46:07 -0700

    Got dynamic team data table to work, data loaded from json file

--- client/src/pages/TeamRankings.js
+++ client/src/pages/TeamRankings.js
@@ -1,11 +1,50 @@
 import MainFooter from "../components/Footer";
 import MainHeader from "../components/Header";
 
+import teamRankingData from '../utils/week3TeamRanking.json';
+console.log(teamRankingData);
+
 // import { useNavigate } from "react-router-dom";
 
 
 const TeamRankings = () => {
 
+  function populateTeamLeaderboardHTML(leaderboardData) {
+
+    var sessionListHTML = []
+
+    // console.log("Leaderboard Data (S): " + JSON.stringify(leaderboardData));
+    // console.log("Leaderboard Data (Rank): " + JSON.stringify(leaderboardData.Rank));
+    // console.log("Leaderboard Data (Team Name): " + JSON.stringify(leaderboardData.Team_Name));
+    // console.log("Leaderboard Data (Points): " + JSON.stringify(leaderboardData.Points));
+    // console.log("Leaderboard Data (S): " + JSON.stringify(leaderboardData));
+    // console.log("Leaderboard Data (Raw) " + leaderboardData);
+
+    
+    //* Create Buttons based user sessions pulled from DB
+    // sessionListHTML.push(<li key={sessionData._id} onClick={(event) => displayItem(event, sessionData.sessionDate, essionData.sessionTime)} className="subMenuBtns m-4 p-2"><div variant="light">{sessionData._id}</div>{' '}</li>)
+    // sessionListHTML.push(<li key={sessionData._id} onClick={(event) => displaySurfSession(event, sessionData._id)} className="previousSurfSessionBTN mt-4 p-3">{sessionData.sessionDate} ({sessionData.sessionTime}) @ {sessionData.sessionLocation}</li>)
+    // console.log(sessionListHTML)
+
+    
+    leaderboardHTML.push(
+      <tr>
+        <td className="teamRankingsTableRank"> {leaderboardData.Rank} </td> 
+        <td className="teamRankingsTableTeamName p-1" > {leaderboardData.Team_Name} </td>
+        <td className="teamRankingsTablePoints p-1"> {leaderboardData.Points} </td>
+      </tr>
+
+    )
+    // leaderboardHTML.push(<td className="teamRankingsTableTeamName p-1" > {leaderboardData.Team_Name} </td> )
+    // leaderboardHTML.push(<td className="teamRankingsTablePoints p-1"> {leaderboardData.Points} </td> )
+  }
+
+  var leaderboardHTML = []
+
+  //* Generate a button for each surf session in the database for the user
+  teamRankingData.forEach(populateTeamLeaderboardHTML)
+
+  console.log(leaderboardHTML)
 
   return (
 
@@ -20,7 +59,7 @@ const TeamRankings = () => {
         <div className="w-100">
 
           {/* <h1 className="text-center mt-5 mx-5 welcomeText">Team Rankings Page!</h1> */}
-          <div className="tableDiv mt-3 px-3">
+          <div className="tableDiv my-3 px-3">
             <table className="teamRankingsTable text-center w-100 mt-3 p-3">
               <thead className="teamRankingsTableTitle">
                 <tr>
@@ -30,120 +69,12 @@ const TeamRankings = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td className="teamRankingsTableRank">1</td>
-                  <td className="teamRankingsTableTeamName">TBD 7</td>
-                  <td className="teamRankingsTablePoints">138</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">2</td>
-                  <td className="teamRankingsTableTeamName">Maximum Effort</td>
-                  <td className="teamRankingsTablePoints">136</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">3</td>
-                  <td className="teamRankingsTableTeamName">Skin Issue</td>
-                  <td className="teamRankingsTablePoints">134</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">4</td>
-                  <td className="teamRankingsTableTeamName">Gravity Crushers</td>
-                  <td className="teamRankingsTablePoints">127</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">5</td>
-                  <td className="teamRankingsTableTeamName">Beta Males</td>
-                  <td className="teamRankingsTablePoints">126</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">5</td>
-                  <td className="teamRankingsTableTeamName">Here to Touch Rocks and Eat Chalk</td>
-                  <td className="teamRankingsTablePoints">126</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">7</td>
-                  <td className="teamRankingsTableTeamName">Idk</td>
-                  <td className="teamRankingsTablePoints">124</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">8</td>
-                  <td className="teamRankingsTableTeamName">GenXSenders</td>
-                  <td className="teamRankingsTablePoints">122</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">8</td>
-                  <td className="teamRankingsTableTeamName">Summertime Sendness</td>
-                  <td className="teamRankingsTablePoints">122</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">10</td>
-                  <td className="teamRankingsTableTeamName">Los Jugs and Pinches</td>
-                  <td className="teamRankingsTablePoints">112</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">10</td>
-                  <td className="teamRankingsTableTeamName">Edge Masters</td>
-                  <td className="teamRankingsTablePoints">112</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">12</td>
-                  <td className="teamRankingsTableTeamName">Team B2</td>
-                  <td className="teamRankingsTablePoints">89</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">13</td>
-                  <td className="teamRankingsTableTeamName">Team C2</td>
-                  <td className="teamRankingsTablePoints">88</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">14</td>
-                  <td className="teamRankingsTableTeamName">Team D2</td>
-                  <td className="teamRankingsTablePoints">87</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">15</td>
-                  <td className="teamRankingsTableTeamName">Team E2</td>
-                  <td className="teamRankingsTablePoints">86</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">16</td>
-                  <td className="teamRankingsTableTeamName">Team F2</td>
-                  <td className="teamRankingsTablePoints">85</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">17</td>
-                  <td className="teamRankingsTableTeamName">Team G2</td>
-                  <td className="teamRankingsTablePoints">84</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">18</td>
-                  <td className="teamRankingsTableTeamName">Team H2</td>
-                  <td className="teamRankingsTablePoints">83</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">19</td>
-                  <td className="teamRankingsTableTeamName">Team I2</td>
-                  <td className="teamRankingsTablePoints">82</td>
-                </tr>
-                <tr>
-                  <td className="teamRankingsTableRank">20</td>
-                  <td className="teamRankingsTableTeamName">Team J2</td>
-                  <td className="teamRankingsTablePoints">81</td>
-                </tr>
+
+                {leaderboardHTML}
+
               </tbody>
             </table>
           </div>
-          
-          {/* <div className="text-center d-flex mb-2 align-items-center justify-content-center">
-          <img src={require("../img/SenderOneLogo.png")}
-            className="footerLogo align-items-center justify-content-center"
-            // style={{paddingLeft: "63px", paddingRight: "63px"}}
-            onClick={() => senderOneHomePage(event)}
-            alt="SenderOne Logo" 
-          />
-          </div> */}
-
-
         </div>
 
       </div>
