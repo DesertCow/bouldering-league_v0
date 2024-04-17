6ebbdf3e76c863d11cbbae0bee83876c00ff3885
Clayton Skaggs <desertcow07@gmail.com>
2024-04-17 15:14:07 -0700

    Cleaned up scorecard page

--- client/src/pages/SubmitScorecard.js
+++ client/src/pages/SubmitScorecard.js
@@ -50,148 +50,150 @@ const SubmitScorecard = () => {
 
         {/* <SideBar/> */}
 
-        <div className="">
+        <div className="w-100 mx-4">
 
-          <h1 className="text-center mt-2 mx-5 welcomeText scorecardTitleText">Week 2</h1>
+          <h1 className="text-center mb-4 mx-5 welcomeText scorecardTitleText">Week 3</h1>
 
-          <table className="teamRankingsTable text-center w-100 mt-3 p-3">
+          <table className="teamRankingsTable text-center w-100">
             <thead className="scorecardTableTitle">
               <tr>
                 <th className="teamRankingsTableRank p-1">Problem #</th>
+                <th className="teamRankingsTableRank p-1">Redpoint</th>
                 <th className="teamRankingsTableRank p-1">Flash?</th>
                 <th className="teamRankingsTableRank p-1">Witness</th>
-                <th className="teamRankingsTableRank p-1">Redpoint</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td className="scorecardTableRank">1</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">2</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">3</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">4</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">5</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">6</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">7</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">8</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">9</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">10</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">11</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">12</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">13</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">14</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">15</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">16</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">17</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">18</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">19</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
               <tr>
                 <td className="scorecardTableRank">20</td>
                 <td className="scorecardTableTeamName p-2"><Switch onChange={flashSwitch} checked={checked} className="react-switch"/></td>
-                <td className="scorecardTableWitness"></td>
-                <td className="scorecardTableWitness"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableTeamName"><Switch onChange={sendSwitch} checked={checked2} className="react-switch"/></td>
+                <td className="scorecardTableWitness"><input id="witness#1" name="witness#1" type="text" /></td>
               </tr>
             </tbody>
           </table>
           
 
-          <div className='py-5 mb-5 text-center'>
-            <Button className="mx-4" onClick={() => scorecardSubmitRequest(event)} variant="success">Submit Scorecard</Button>{' '}
-            <Button className="mx-4" onClick={() => scorecardClear(event)} variant="warning">Clear Scorecard</Button>{' '}
+          <div className='scorcardSubmitButtonGroup text-center mt-5 d-flex flex-row'>
+            <Button className="mx-5 scorecardButton" onClick={() => scorecardSubmitRequest(event)} variant="success">Save Scorecard <br></br>(Locally)</Button>{' '}
+            <Button className="mx-5 scorecardButton" onClick={() => scorecardSubmitRequest(event)} variant="warning">Submit Scorecard</Button>{' '}
+            <Button className="mx-5 scorecardButton" onClick={() => scorecardClear(event)} variant="danger">Clear Scorecard</Button>{' '}
           </div>
+
           {/* <div className="text-center d-flex mb-2 align-items-center justify-content-center">
           <img src={require("../img/SenderOneLogo.png")}
             className="footerLogo align-items-center justify-content-center"
